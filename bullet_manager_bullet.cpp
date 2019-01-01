7e4aa07c98edc015f746887653d4e5afb58fc782
Michael Abramowski <gorgexpress@gmail.com>
2019-01-01 15:43:11 -0600

    support for changing bullet properties from gdscript

--- bullet_manager_bullet.cpp
+++ bullet_manager_bullet.cpp
@@ -20,20 +20,83 @@ void BulletManagerBullet::_body_inout(int p_status, const RID &p_body, int p_ins
 	type->body_inout(this, p_status, p_body, p_instance, p_body_shape, p_area_shape );
 }
 
-Point2 BulletManagerBullet::get_global_position() {
+void BulletManagerBullet::set_position(Point2 position) {
+	matrix.set_origin(position);
+}
+
+Point2 BulletManagerBullet::get_position() const {
 	return matrix.get_origin();
 }
 
-void BulletManagerBullet::set_global_position(Point2 position) {
-	matrix.set_origin(position);
+
+
+void BulletManagerBullet::set_direction(Vector2 direction) {
+    this->direction = direction;
+}
+
+Vector2 BulletManagerBullet::get_direction() const {
+    return direction;
+}
+
+
+void BulletManagerBullet::set_speed(real_t speed) {
+    this->speed = speed;
+}
+
+real_t BulletManagerBullet::get_speed() const {
+    return speed;
+}
+
+void BulletManagerBullet::set_acceleration(real_t acceleration) {
+    this->acceleration = acceleration;
+}
+
+real_t BulletManagerBullet::get_acceleration() const {
+    return acceleration;
+}
+
+
+void BulletManagerBullet::set_type(Node* bullet_manager_bullet_type) {
+    //Setting the type to one belonging to a different BulletManager isn't supported, and I don't see a good reason to support it.
+    ERR_FAIL_COND(this->type->get_parent() != type->get_parent());
+    BulletManagerBulletType* type = Object::cast_to<BulletManagerBulletType>(bullet_manager_bullet_type);
+    Physics2DServer* ps = Physics2DServer::get_singleton();
+    if (!is_queued_for_deletion) {
+        //Don't want to undo the layer and mask being set to 0 when scheduled for deletion
+        //Honestly, returning immediately if the bullet is queued for deletion might be a better idea. 
+        ps->area_set_collision_layer(area, type->get_collision_layer());
+	    ps->area_set_collision_mask(area, type->get_collision_mask());
+    }
+	
+    ps->area_clear_shapes(area);
+    ps->area_add_shape(area, type->get_collision_shape()->get_rid());
+    this->type = type;
+}
+
+Node* BulletManagerBullet::get_type() const  {
+    return type;
 }
 
 void BulletManagerBullet::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("_body_inout"), &BulletManagerBullet::_body_inout);
 	ClassDB::bind_method(D_METHOD("_area_inout"), &BulletManagerBullet::_area_inout);
-	ClassDB::bind_method(D_METHOD("set_global_position"), &BulletManagerBullet::set_global_position);
-	ClassDB::bind_method(D_METHOD("get_global_position"), &BulletManagerBullet::get_global_position);
+    ClassDB::bind_method(D_METHOD("set_position"), &BulletManagerBullet::set_position);
+	ClassDB::bind_method(D_METHOD("get_position"), &BulletManagerBullet::get_position);
+	ClassDB::bind_method(D_METHOD("set_direction", "direction"), &BulletManagerBullet::set_direction);
+	ClassDB::bind_method(D_METHOD("get_direction"), &BulletManagerBullet::get_direction);
+    ClassDB::bind_method(D_METHOD("set_speed", "speed"), &BulletManagerBullet::set_speed);
+	ClassDB::bind_method(D_METHOD("get_speed"), &BulletManagerBullet::get_speed);
+    ClassDB::bind_method(D_METHOD("set_acceleration", "acceleration"), &BulletManagerBullet::set_acceleration);
+	ClassDB::bind_method(D_METHOD("get_acceleration"), &BulletManagerBullet::get_acceleration);
+    ClassDB::bind_method(D_METHOD("set_type", "bullet_manager_bullet_type"), &BulletManagerBullet::set_type);
+	ClassDB::bind_method(D_METHOD("get_type"), &BulletManagerBullet::get_type);
     ClassDB::bind_method(D_METHOD("queue_delete"), &BulletManagerBullet::queue_delete);
-	ADD_PROPERTY(PropertyInfo(Variant::VECTOR2, "global_position", PROPERTY_HINT_NONE), "set_global_position", "get_global_position");
+
+    //I'm assuming I don't need property hints for this...
+	ADD_PROPERTY(PropertyInfo(Variant::VECTOR2, "position", PROPERTY_HINT_NONE), "set_position", "get_position");
+    ADD_PROPERTY(PropertyInfo(Variant::VECTOR2, "direction", PROPERTY_HINT_NONE), "set_direction", "get_direction");
+    ADD_PROPERTY(PropertyInfo(Variant::REAL, "speed", PROPERTY_HINT_NONE), "set_speed", "get_speed");
+    ADD_PROPERTY(PropertyInfo(Variant::REAL, "acceleration", PROPERTY_HINT_NONE), "set_acceleration", "get_acceleration");
+    ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "type", PROPERTY_HINT_NONE), "set_type", "get_type");
 
 }
--- bullet_manager_bullet.h
+++ bullet_manager_bullet.h
@@ -13,7 +13,7 @@ class BulletManagerBullet : public Object {
     Transform2D matrix;
 	Vector2 direction;
 	real_t speed = 0;
-	real_t acceleration;
+	real_t acceleration = 0;
 	RID area;
 	bool is_queued_for_deletion = false;
 	BulletManagerBulletType* type;
@@ -22,8 +22,16 @@ public:
 
 	void _area_inout(int p_status, const RID &p_area, int p_instance, int p_area_shape, int p_self_shape);
 	void _body_inout(int p_status, const RID &p_body, int p_instance, int p_body_shape, int p_area_shape);
-	Point2 get_global_position();
-	void set_global_position(Point2 position);
+	void set_position(Point2 position);
+	Point2 get_position() const;
+    void set_direction(Vector2 direction);
+    Vector2 get_direction() const;
+    void set_speed(real_t speed);
+    real_t get_speed() const;
+    void set_acceleration(real_t acceleration);
+    real_t get_acceleration() const;
+    void set_type(Node* bullet_manager_bullet_type);
+    Node* get_type() const;
     void queue_delete();
 protected:
 	static void _bind_methods();
